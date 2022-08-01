a34acd43c16e930a3695d7b1f658609969ff10e4
muhammadhassam2022skipq <muhammad.hassam.skipq@gmail.com>
2022-08-01 21:32:02 +0500

    bycrpt + Validation

--- controller/accountsController.js
+++ controller/accountsController.js
@@ -2,17 +2,22 @@ const express = require ("express");
 const mongoose = require ("mongoose");
 const signUPModel = require("../model/signUPModel");
 const bcrypt = require("bcrypt");
+const {validationResult} = require('express-validator/check');
 // sign up
 
 exports.getSignUP = ((req,res)=> {
     const isAuthenticated = req.session.isLoggedIn ? req.session.isLoggedIn : false;
     res.render("signup", {
         pageTitle: "Sign Up",
-        isAuthenticated: isAuthenticated
+        isAuthenticated: isAuthenticated,
+        validationErrors: []
     })
 })
 
 exports.postSignUP = ((req,res)=> {
+    const errors = validationResult(req);
+    const isAuthenticated = req.session.isLoggedIn ? req.session.isLoggedIn : false;
+    if(errors.isEmpty()) {
     bcrypt.hash(req.body.password, 10).then ((hash)=>{
         const signUP = new signUPModel ({
             _id: mongoose.Types.ObjectId(),
@@ -20,7 +25,6 @@ exports.postSignUP = ((req,res)=> {
             lastName: req.body.lastName,
             email: req.body.email,
              password: hash
-    
         })
         signUP.save().then((signedUPUer)=>{
             console.log ("successfuly saved"+signedUPUer)
@@ -29,7 +33,17 @@ exports.postSignUP = ((req,res)=> {
         });
     }).catch((err)=> {
         console.log ("The hash error is: "+ err);
-    });
+    })
+} else {
+    res.render('signup', {
+        path:'/signup',
+        pageTitle: 'Sign Up',
+        errors: errors,
+        validationErrors: errors.array(),
+        isAuthenticated: isAuthenticated
+    })
+
+}
     
 })
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,10 +1,26 @@
 const express = require ("express");
 const accountsController = require("../controller/accountsController");
+const { check } = require('express-validator');
 const router = express.Router();
 
 
 router.get ("/signup", accountsController.getSignUP);
-router.post ("/signup", accountsController.postSignUP);
+router.post ("/signup",
+// check(req.body.firstName)
+// .isAlphanumeric()
+// .withMessage('Name cannot have a number'),
+// check(req.body.lastName)
+// .isAlphanumeric()
+// .withMessage('Name cannot have a number'),
+check('email')
+.isEmail()
+.withMessage('Not an email'),
+check('password')
+.isLength({ min: 5 })
+.withMessage('must be at least 5 chars long')
+.matches(/\d/)
+.withMessage('must contain a number'),
+accountsController.postSignUP);
 router.get ("/signin", accountsController.getSignIn);
 router.post ("/signin", accountsController.postSignIn);
 
